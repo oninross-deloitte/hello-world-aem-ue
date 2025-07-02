5047f6da5d984568f08725dcbcaa73024fe42a6f
Nino Rodriguez <nirodriguez@deloitte.com.au>
2025-07-02 21:41:17 +1000

    Add .prettierrc.js configuration for code formatting and update .editorconfig for consistent indentation

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // { foo: bar } as opposed to {foo:bar}, for readability
+  bracketSpacing: true,
+
+  // Single quotes for strings, except in JSX
+  singleQuote: true,
+  quoteProps: 'consistent',
+
+  // Trailing commas for improved diff
+  trailingComma: 'all',
+
+  // enforce LF for line breaks
+  endOfLine: 'lf',
+};
